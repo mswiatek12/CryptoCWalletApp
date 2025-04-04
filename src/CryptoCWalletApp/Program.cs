e567de53ec2e61268efca4fe377deaef7d085de9
Maksymilian Swiatek <maksymilian143@gmail.com>
2025-04-04 17:39:27 +0200

    Initial commit

---
+++ src/CryptoCWalletApp/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+{
+    //scope for configuring services (Dependency Injection)
+}
+
+var app = builder.Build();
+{
+    //configure request pipeline
+}
+
+app.Run();
